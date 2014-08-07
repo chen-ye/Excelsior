555ac57c22aa0dbc785e11a426986f9a05acb593
Chen Ye <cye@brown.edu>
2014-08-07 01:29:08 -0400

    ggmotttt

--- Postprocessed_Data/HHMI_climb.r
+++ Postprocessed_Data/HHMI_climb.r
@@ -1,6 +1,6 @@
 ### New files from Adam 8/6/14  all_grouped_medians.csv is for automated track data from Chen Ye's program with every instantaneous velocity (a derivitive across 1/15 second intervals of dY/dT for about 20-25 seconds).  The excel file is the manual tracked file from Adam, some rows are for 0, 3, 6, 9 cm.  Pixels go in reverse order so 0s have larger Y value sfo rhte pixel count.  Ecel file velocities need to be sclaed differently due to camera distance.  Young scalar = 0.02868. old scalar = 0.0282.  last column is this multiplied by Dy_abs value in excel file.
 
-velauto <- read.csv("/Users/davidrand/Documents/Grants/HHMI Undergraduate/2014 Project/HHMIclimbing/all_grouped_medians.csv", colClasses=c(rep('factor',5), rep('numeric',15)))
+velauto <- read.csv("~/FlyLapse/Postprocessed_Data/all_grouped_medians.csv", colClasses=c(rep('factor',5), rep('numeric',15)))
 head(velauto)
 plot(velauto$Velo_mean_cm_sec, velauto$Velo_median_cm_sec)
 summary(velauto)
@@ -32,10 +32,13 @@ boxplot(Velo_mean_cm_sec ~ Age*Mito, data=velpostrim)
 
 summary(velpostrim)
 
+#Restyled boxplots
+boxplot(Velo_median_cm_sec ~ Age*Nuclear*Mito, data=velpostrim, las=2, frame=T, xlab="Age.Nuclear.Mito", ylab="Climbing velocity(cm/sec)", main="Auto Climbing speed (Outliers trimmed)", col=(c("green3", "green3", "red", "red", "thistle", "thistle", "steelblue1", "steelblue1", "orchid", "orchid", "turquoise4", "turquoise4", "white", "white", "yellow", "yellow", "orange", "orange", "rosybrown1", "rosybrown1", "darkseagreen2", "darkseagreen2", "gray60", "gray60")))
 
+ggplot(velpostrim, aes(x=Nuclear, fill = Mito, color = Mito, alpha = factor(Age), y=Velo_median_cm_sec)) + ylim(0, 2.5) + geom_boxplot()
 
 ### Manual data analyzed here
-velman <- read.csv("/Users/davidrand/Documents/Grants/HHMI Undergraduate/2014 Project/HHMIclimbing/Manual_Fly_Tracks.csv", colClasses=c(rep('factor',9), rep('numeric',4)))
+velman <- read.csv("~/FlyLapse/Postprocessed_Data/Manual_Fly_Tracks.csv", colClasses=c(rep('factor',9), rep('numeric',4)))
 head(velman)
 velman3 <- velman[ which(velman$Time..sec. == "3"), ]
 velman6 <- velman[ which(velman$Time..sec. == "6"), ]
@@ -56,7 +59,10 @@ summary(velman9aov)
 boxplot(veloscaled ~ Age*Nuclear*Mito, data=velman, las=2, frame=T, xlab="Age.Nuclear.Mito", ylab="Climbing velocity(cm/sec)", col=(c("green3", "green3", "red", "red", "thistle", "thistle", "steelblue1", "steelblue1", "orchid", "orchid", "turquoise4", "turquoise4", "white", "white", "yellow", "yellow", "orange", "orange", "rosybrown1", "rosybrown1", "darkseagreen2", "darkseagreen2", "gray60", "gray60")), names=c("Old 15B", "Young 15B", "Old 15C", "Young 15C", "Old 15G", "Young 15G", "Old 15I", "Young 15I", "Old 19B", "Young 19B", "Old 19C", "Young 19C", "Old 19G", "Young 19G", "Old 19I", "Young 19I", "Old 3B", "Young 3B", "Old 3C", "Young 3C", "Old 3G", "Young 3G", "Old 3I", "Young 3I"))
 boxplot(veloscaled ~ Mito+Nuclear+Age, data=velman3, las=2, xlab="Age.Nuclear.Mito", ylab="Climbing velocity(cm/sec)", col=(c("green3", "green3", "red", "red", "thistle", "thistle", "steelblue1", "steelblue1", "orchid", "orchid", "turquoise4", "turquoise4", "white", "white", "yellow", "yellow", "orange", "orange", "rosybrown1", "rosybrown1", "darkseagreen2", "darkseagreen2", "gray60", "gray60")), names=c("Old 15B", "Young 15B", "Old 15C", "Young 15C", "Old 15G", "Young 15G", "Old 15I", "Young 15I", "Old 19B", "Young 19B", "Old 19C", "Young 19C", "Old 19G", "Young 19G", "Old 19I", "Young 19I", "Old 3B", "Young 3B", "Old 3C", "Young 3C", "Old 3G", "Young 3G", "Old 3I", "Young 3I"))
 boxplot(veloscaled ~ Age*Nuclear*Mito, data=velman9, las=2, frame=T, xlab="Age.Nuclear.Mito", ylab="Climbing velocity(cm/sec)", main="Climbing speed at 9cm", col=(c("green3", "green3", "red", "red", "thistle", "thistle", "steelblue1", "steelblue1", "orchid", "orchid", "turquoise4", "turquoise4", "white", "white", "yellow", "yellow", "orange", "orange", "rosybrown1", "rosybrown1", "darkseagreen2", "darkseagreen2", "gray60", "gray60")), names=c("O 15B", "Y 15B", "O 15C", "Y 15C", "O 15G", "Y 15G", "O 15I", "Y 15I", "O 19B", "Y 19B", "O 19C", "Y 19C", "O 19G", "Y 19G", "O 19I", "Y 19I", "O 3B", "Y 3B", "O 3C", "Y 3C", "O 3G", "Y 3G", "O 3I", "Y 3I"))
-plot(velman6$veloscaled, velman9$veloscaled)
+
+MitoNuc <- interaction(velman$Nuclear, velman$Mito, sep=" ")
+
+ggplot(velman, aes(x=Nuclear, fill = Mito, color = Mito, alpha = factor(Age), y=veloscaled)) + ylim(0, 2.5) + geom_boxplot()
 
 #stack overflow answer to moving  x lable position on box plot
 boxplot(c(1:12)~c(rep("1",6),rep("2",6)),at=c(1,2), col=c(0,"grey"),las=1,xaxt="n") 
